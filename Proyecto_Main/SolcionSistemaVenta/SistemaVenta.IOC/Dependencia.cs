3a9dc020f6aa3c170ecb2a61c0e49e73d195353c
MainRDSniper09 <barretosanchez6@gmail.com>
2024-10-04 22:25:59 -0500

    Ingresamos nuestra cadena de conexion dentro de nuestra clase Dependencia, para esto creamos un metodo

---
+++ Proyecto_Main/SolcionSistemaVenta/SistemaVenta.IOC/Dependencia.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Agregamos todas las referencias
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SistemaVenta.DALL.DBContext;
+using Microsoft.EntityFrameworkCore;
+//using SistemaVenta.DALL.Interfaces;
+//using SistemaVenta.DALL.Implementacion;
+//using SistemaVenta.BLL.Interfaces;
+//using SistemaVenta.BLL.Implementacion;
+
+namespace SistemaVenta.IOC
+{
+    public static class Dependencia
+    {
+        // Logica
+        public static void InyectarDependencia(this IServiceCollection services, IConfiguration configuration) {
+            services.AddDbContext<DBVENTAContext>(options => {
+                options.UseSqlServer(configuration.GetConnectionString("CadenaSQL")); // Ponemos nuestra cadena de conexion
+            });
+        }
+
+    }
+}
