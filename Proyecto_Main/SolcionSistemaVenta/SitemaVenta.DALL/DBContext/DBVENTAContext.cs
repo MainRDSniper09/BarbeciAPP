6747b3e33c5bc78f195da65f3f6738e68fa5e040
MainRDSniper09 <barretosanchez6@gmail.com>
2024-10-04 22:08:23 -0500

    Se borra la cadena de conexion y se mueve a nuestro archivo json

--- Proyecto_Main/SolcionSistemaVenta/SitemaVenta.DALL/DBContext/DBVENTAContext.cs
+++ Proyecto_Main/SolcionSistemaVenta/SitemaVenta.DALL/DBContext/DBVENTAContext.cs
@@ -32,11 +32,6 @@ namespace SistemaVenta.DALL.DBContext  // Agregamos una referencia hacia nuestra
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=(local); DataBase=DBVENTA;Integrated Security=true");
-            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
