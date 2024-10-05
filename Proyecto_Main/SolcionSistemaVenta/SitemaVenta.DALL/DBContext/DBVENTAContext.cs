3da214b1182e1b81c8b6a44181dfd32cca7ce638
MainRDSniper09 <barretosanchez6@gmail.com>
2024-10-04 21:59:16 -0500

    Se agrega la referencia a hacia Entity

--- Proyecto_Main/SolcionSistemaVenta/SitemaVenta.DALL/DBContext/DBVENTAContext.cs
+++ Proyecto_Main/SolcionSistemaVenta/SitemaVenta.DALL/DBContext/DBVENTAContext.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
-namespace SistemaVenta.Entity
+namespace SistemaVenta.DALL.DBContext  // Agregamos una referencia hacia nuestra capa Entity
 {
     public partial class DBVENTAContext : DbContext
     {
